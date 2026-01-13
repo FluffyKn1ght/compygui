02cf078d0ed78928c1276254e8b9703c58aa059f
FluffyKn1ght <lightcraftzofficial@gmail.com>
2026-01-13 21:14:31 +0300

    Fix circular import

--- compygui/viewport.py
+++ compygui/viewport.py
@@ -1,12 +1,9 @@
 from compygui.component import Component
-from compygui.compygui import ComPyGUIApp
-from compygui.window import Window
 
 
 class Viewport(Component):
-    def __init__(self, *args, window: Window, **kwargs) -> None:
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.window = window
 
     def destroy(self) -> None:
         pass
