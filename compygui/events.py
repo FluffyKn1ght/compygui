b1e76c7d82e46741a52da771b2e99daf8dbc3070
FluffyKn1ght <lightcraftzofficial@gmail.com>
2026-01-15 14:19:34 +0300

    Migrate event listeners to an EventListenerRecord class

--- compygui/events.py
+++ compygui/events.py
@@ -1,3 +1,5 @@
+import uuid
+
 from enum import Enum
 from typing import Any, Callable
 
@@ -28,43 +30,63 @@ class Event:
         type: str,
         *args,
         event_expires_in: int = 1,
-        event_origin: Component | EventOrigin = EventOrigin.UNKNOWN,
-        **evdata
+        event_origin: Component | EventOrigin,
+        **evdata,
     ) -> None:
         self.type: str = type
         self.data: dict[str, Any] = evdata
         self.expires_in: int = event_expires_in
         self.origin: Component | EventOrigin = event_origin
 
+        self.age: int = 0
+
+
+class EventListener:
+    def __init__(
+        self,
+        *args,
+        callback: Callable[[Event]],
+        type: str,
+        condition: Callable[[Event], bool] | None,
+        oneshot: bool,
+        disconnect: Callable[[EventListener]],
+    ) -> None:
+        self.callback: Callable[[Event]] = callback
+        self.condition: Callable[[Event], bool] | None = condition
+        self.type: str = type
+        self.oneshot: bool = oneshot
+        self._disconnect: Callable[[EventListener]] = disconnect
+
+        self.uuid: uuid.UUID = uuid.uuid4()
+        self.valid: bool = True
+
+    def disconnect(self) -> None:
+        self.valid = False
+        self._disconnect(self)
+
 
 class EventQueue:
     def __init__(self):
         self.events: list[Event] = []
-        self._listeners: list[dict[str, Any]] = []
+        self._listeners: list[EventListener] = []
 
     def listen(
         self,
+        callback: Callable[[Event]],
         for_event: str,
         condition: Callable[[Event], bool] | None = None,
         *args,
-        oneshot: bool = False
-    ) -> Callable:
-        def register_listener(func: Callable) -> Callable:
-            def listen_wrapper(event: Event, *args, **kwargs) -> Any:
-                return func(event)
-
-            self._listeners.append(
-                {
-                    "func": func,
-                    "type": for_event,
-                    "check": condition,
-                    "oneshot": oneshot,
-                }
-            )
-
-            return listen_wrapper
-
-        return register_listener
+        oneshot: bool = False,
+    ) -> EventListener:
+        listener: EventListener = EventListener(
+            callback=callback,
+            type=for_event,
+            condition=condition,
+            oneshot=oneshot,
+            disconnect=lambda x: self.disconnect(x),
+        )
+        self._listeners.append(listener)
+        return listener
 
     def fire(self, evtype: str, *args, **evdata) -> None:
         if len(self.events) >= 1024:
@@ -78,15 +100,32 @@ class EventQueue:
         event: Event = Event(evtype, *args, **evdata)
         self.events.append(event)  # TODO: Make events expire
 
-        idx: int = -1
         for listener in self._listeners:
-            idx += 1
-            if listener["type"] == event.type:
-                if listener["check"] != None:
-                    if not listener["check"](event):
+            if listener.type == event.type:
+                if listener.condition != None:
+                    if not listener.condition(event):
                         continue
 
-                listener["func"](event)
+                if not listener.valid:
+                    raise ValueError(
+                        f"[ComPyGUI BUG] Invalid (disconnected) listener in EventQueue()._listeners at index {self._listeners.index(listener)}"
+                    )
 
-                if listener["oneshot"]:
-                    del self._listeners[idx]
+                listener.callback(event)
+
+                if listener.oneshot:
+                    listener.disconnect()
+
+    def disconnect(self, listener_or_uuid: EventListener | uuid.UUID) -> None:
+        listener: EventListener | None = None
+        if type(listener_or_uuid) is EventListener:
+            listener = listener_or_uuid
+        elif type(listener_or_uuid) is uuid.UUID:
+            for lis in self._listeners:
+                if lis.uuid == listener_or_uuid:
+                    listener = lis
+
+        if listener is None:
+            raise ValueError(f"Unable to find listener from UUID {listener_or_uuid}")
+
+        self._listeners.remove(listener)
